3f9b2d40fd8090a1e11c15a739c78ce39780798c
CodeGuiFs <100089821+CodeGuiFs@users.noreply.github.com>
2022-05-07 22:30:41 -0300

    Alteração de Funções
    - Substituição do método onclick dentro do HTML.
    - Inclusão de Event Listeners para ações com mouse e teclado.

--- script.js
+++ script.js
@@ -1,98 +1,126 @@
-var txt = document.getElementById('rescalc')
+var visorCalc = document.querySelector("textarea");
+var btn = document.querySelectorAll("input[type=button]");
 
-function diginum(a){
-    switch(a){
-        case 0:
-            txt.innerHTML += `${a}`  
-            break
-        case 1:
-            txt.innerHTML += `${a}`
-            break
-        case 2:
-            txt.innerHTML += `${a}`
-            break
-        case 3:
-            txt.innerHTML += `${a}`
-            break
-        case 4:
-            txt.innerHTML += `${a}`
-            break
-        case 5:
-            txt.innerHTML += `${a}`
-            break
-        case 6:
-            txt.innerHTML += `${a}`
-            break
-        case 7:
-            txt.innerHTML += `${a}`
-            break
-        case 8:
-            txt.innerHTML += `${a}`
-            break
-        case 9:
-            txt.innerHTML += `${a}`
-            break
-        case 10:
-            txt.innerHTML += `/`
-            break
-        case 11:
-            txt.innerHTML += `*`
-            break
-        case 12:
-            txt.innerHTML += `-`
-            break
-        case 13:
-            txt.innerHTML += `+`
-            break
-        case 14:
-            txt.innerHTML += `.`
-            break
-        case 15:
-            txt.innerHTML = ``
-            break
-        case 16:
-            txt.innerHTML += `,`
-            break
-        case 17:
-            txt.innerHTML += `=`
+visorCalc.addEventListener("click", function(){
+    this.blur();
+});
+
+for (var c = 0; c < btn.length; c++) {
+    btn[c].addEventListener("click", function () {
+        this.blur();
+        return diginum(this.value);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    return diginum(event.key)
+});
+
+
+function diginum(num) {
+    switch (num) {
+        case "0":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "1":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "2":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "3":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "4":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "5":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "6":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "7":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "8":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "9":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "/":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "*":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "-":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case "+":
+            visorCalc.insertAdjacentHTML("beforeend", num);
+            break;
+        case ".":
+            visorCalc.insertAdjacentHTML("beforeend", ".");
+            break;
+        case ",":
+            visorCalc.insertAdjacentHTML("beforeend", ".");
+            break;
+        case "Enter":
+            visorCalc.insertAdjacentHTML("beforeend", "=");
             calcular()
+            break;
+        case "=":
+            visorCalc.insertAdjacentHTML("beforeend", "=");
+            calcular()
+            break;
+        case "C":
+            visorCalc.innerHTML = ``;
             break
+        case "Delete":
+            visorCalc.innerHTML = ``;
+            break;
     }
 }
 
-function calcular(){
-    var num = txt.value
-    var txtcalc = txt.value.length
-    var valor1 = ""
-    var valor2 = ""
-    var v =""
-    var op = ""
-for(var i = 0; i < txtcalc; i++){
-    var c = num[i]
-    if(c == "+" || c == "-" || c == "*" || c == "/" || c == "**"){
-        valor1 = v
-        op = c
-        v = ""
-    }else if(c == "="){
-        valor2 = v
-        break
-    }
-    else{
-        v += c
-    }
-}
-var nvalor1 = Number(valor1)
-var nvalor2 = Number(valor2)
-var res = 0
-if(op == "+"){
-    res = nvalor1 + nvalor2
-}else if(op == "-"){
-    res = nvalor1 - nvalor2
-}else if(op == "*"){
-    res = nvalor1 * nvalor2
-}else if(op == "/"){
-    res = nvalor1 / nvalor2
-}
-   txt.innerHTML = `${res}`
 
+
+function calcular() {
+    var textCalc = document.querySelector("textarea").value; //Recebe a String digitada na calculadora.
+    var valor1 = ""                                          //Armazena o primeiro valor. 
+    var valor2 = ""                                          //Armazena o segundo valor.
+    var conjuntoCaracter = ""                                //Armazena os caracteres individuais da String.
+    var sinalOperacao = ""                                   //Armazena o caracter de operação. 
+
+    for (var i = 0; i < textCalc.length; i++) {
+        var c = textCalc[i]
+        if (c == "+" || c == "-" || c == "*" || c == "/") {
+            valor1 = conjuntoCaracter;
+            sinalOperacao = c;
+            conjuntoCaracter = "";
+        } else if (c == "=") {
+            valor2 = conjuntoCaracter;
+            break;
+        } else {
+            conjuntoCaracter += c;
+        }
+    }
+
+    var nvalor1 = Number(valor1);
+    var nvalor2 = Number(valor2);
+    var resultado = 0;
+    
+        if (sinalOperacao == "+") {
+            resultado = nvalor1 + nvalor2;
+        } else if (sinalOperacao == "-") {
+            resultado = nvalor1 - nvalor2;
+        } else if (sinalOperacao == "*") {
+            resultado = nvalor1 * nvalor2;
+        } else if (sinalOperacao == "/") {
+            resultado = nvalor1 / nvalor2;
+        }
+    
+    
+    visorCalc.innerHTML = `${resultado}`;
 }
